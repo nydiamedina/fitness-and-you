ed352c33829c47393e7194672b538e0b9830d342
Nydia Medina <nvmedina29@gmail.com>
2022-10-11 15:49:21 -0600

    Integrating get and post workout endpoints in Front.

--- client/main.js
+++ client/main.js
@@ -1,74 +1,104 @@
-let newWorkoutModal = document.querySelector("#new-workout-modal");
-let closeNewWorkoutButton = document.querySelector("#close-new-workout-btn");
+const newWorkoutModal = document.querySelector("#new-workout-modal");
+const saveNewWorkoutButton = document.querySelector("#save-new-workout-btn");
+const closeNewWorkoutButton = document.querySelector("#close-new-workout-btn");
+const calendarEl = document.querySelector('#calendar');
+const workoutForm = document.querySelector('#workout-form');
+const workoutTitleInput = document.querySelector('#input-title');
+const workoutDateInput = document.querySelector('#input-date');
+const workoutStartTimeInput = document.querySelector('#input-start-time');
+const workoutDurationInput = document.querySelector('#input-duration');
+const workoutCaloriesInput = document.querySelector('#input-calories');
 
-let newWorkoutDateInput = document.querySelector("#inputDate");
+const baseURL = 'http://localhost:4004';
+// TODO: This userId should be obtained from an authentication function
+const userId = '1';
 
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialDate: Date.now(),
-    editable: false,
-    selectable: true,
-    businessHours: false,
-    dayMaxEvents: true, // allow "more" link when too many events
-    select: function(arg) {
-      newWorkoutModal.style.display = "block";
-      newWorkoutDateInput.value = arg.start.toLocaleDateString();
-    },
-    events: [
-      {
-        title: 'All Day Event',
-        start: '2022-10-01'
-      },
-      {
-        title: 'Long Event',
-        start: '2022-10-07',
-        end: '2022-10-10'
-      },
-      {
-        title: 'Conference',
-        start: '2022-10-11',
-        end: '2022-10-13'
-      },
-      {
-        title: 'Meeting',
-        start: '2022-10-12T10:30:00',
-        end: '2022-10-12T12:30:00'
-      },
-      {
-        title: 'Lunch',
-        start: '2022-10-12T12:00:00'
-      },
-      {
-        title: 'Meeting',
-        start: '2022-10-12T14:30:00'
-      },
-      {
-        title: 'Happy Hour',
-        start: '2022-10-12T17:30:00'
-      },
-      {
-        title: 'Dinner',
-        start: '2022-10-12T20:00:00'
-      },
-      {
-        title: 'Birthday Party',
-        start: '2022-10-13T07:00:00'
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2022-10-28'
-      }
-    ]
-  });
-
-  calendar.render();
+let workouts = [];
+let calendar = new FullCalendar.Calendar(calendarEl, {
+  initialDate: Date.now(),
+  editable: false,
+  selectable: true,
+  businessHours: false,
+  dayMaxEvents: true, // allow "more" link when too many events
+  select: function(arg) {
+    newWorkoutModal.style.display = "block";
+    workoutDateInput.value = arg.start.toLocaleDateString();
+  },
+  events: workouts
 });
 
-function closeModal() {
+console.log(calendar);
+
+function closeWorkoutModal() {
   newWorkoutModal.style.display = "none";
 }
 
-closeNewWorkoutButton.addEventListener('click', closeModal);
+function getWorkouts() {
+  axios.get(`${baseURL}/api/users/${userId}/workouts`)
+  .then(res => {
+    workouts = res.data.map((w) => {
+      return {
+        id: w.id,
+        title: w.title,
+        start: w.start_time,
+        end: w.end_time
+      }
+    });
+    
+    for(let i = 0; i < workouts.length; i++) {
+      calendar.addEvent(workouts[i]);
+    }
+  });
+}
+
+function addWorkout(e) {
+  e.preventDefault()
+
+  if (!workoutTitleInput.value) {
+    alert ('You must enter a workout title.');
+    return;
+  }
+
+  if (!workoutStartTimeInput.value) {
+    alert ('You must enter a workout start time.');
+    return;
+  }
+
+  if (!workoutDurationInput.value) {
+    alert ('You must enter a workout duration.');
+    return;
+  }
+
+  let startTime = new Date(workoutDateInput.value);
+  let [hours, minutes] = workoutStartTimeInput.value.split(":");
+  startTime.setHours(hours, minutes);
+
+  let endTime = new Date();
+  endTime.setTime(startTime.getTime() + workoutDurationInput.value * 60000);
+
+  let body = {
+    user_id: userId,
+    title: workoutTitleInput.value,
+    start_time: startTime,
+    end_time: endTime,
+    duration: workoutDurationInput.value,
+    calories: workoutCaloriesInput.value
+  }
+
+  axios.post(`${baseURL}/api/users/${userId}/workouts`, body)
+  .then(()=> {
+    calendar.addEvent({
+      title: body.title,
+      start: body.start_time,
+      end: body.end_time
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  getWorkouts();
+  calendar.render();
+});
+
+closeNewWorkoutButton.addEventListener('click', closeWorkoutModal);
+workoutForm.addEventListener('submit', addWorkout);
--- server/index.js
+++ server/index.js
@@ -16,15 +16,15 @@ app.use(express.json());
 app.use(cors());
 
 // USERS
-app.get('/users/:userId', getUser);
-app.put('/users/:userId', updateUser);
+app.get('/api/users/:userId', getUser);
+app.put('/api/users/:userId', updateUser);
 
 // WORKOUTS
-app.get('/users/:userId/workouts', getUserWorkouts);
-app.post('/users/:userId/workouts', createUserWorkout);
-app.delete('/users/:userId/workouts/:workoutId', deleteUserWorkout);
+app.get('/api/users/:userId/workouts', getUserWorkouts);
+app.post('/api/users/:userId/workouts', createUserWorkout);
+app.delete('/api/users/:userId/workouts/:workoutId', deleteUserWorkout);
 
 // GENDERS
-app.get('/genders', getGenders);
+app.get('/api/genders', getGenders);
 
 app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
